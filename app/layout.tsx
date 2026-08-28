633a3bc7b71e1c6e1494a9a591ab374f38afc688
7mud2007a <bznsman77@gmail.com>
2026-08-28 14:27:38 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
